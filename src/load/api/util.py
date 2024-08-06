d5aea36baa42d9033c8394534cf0eced1fafd48c
tbongkim03 <tbongkim03@gmial.com>
2024-08-06 14:46:31 +0900

    update

--- src/load/api/util.py
+++ src/load/api/util.py
@@ -3,7 +3,7 @@ import pandas as pd
 def tabulate_df(ds_nodash):
     year = ds_nodash[0:4]
     month = ds_nodash[4:6]
-    read_df = pd.read_parquet(f'~/t2/test_parquet/year={year}/month={month}')
+    read_df = pd.read_parquet(f'~/t2/test_parquet/ordered_parquet/year={year}/month={month}')
     headers = ['movieCd', 'movieNm', 'audiCnt', 'year', 'month']
     tabulate.WIDE_CHARS_MODE = False
     return tabulate(df_cleaned, headers, tablefmt="rst")
